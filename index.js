f1c7492ad6870b196a5cfb771b3e0a45683a4cfc
sulakshana borsune <sulakshana.borsune@gmail.com>
2020-02-23 18:06:40 -0800

    made some changes

--- index.js
+++ index.js
@@ -1,62 +1,64 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 const util = require('util')
+var path = require('path')
 const writeFileAsync = util.promisify(fs.writeFile)
 
-class Employee{
-    constructor(name,id,title){
+class Employee {
+    constructor(name, id, title) {
         this.name = name
         this.id = id
         this.title = title
     }
 
-getName(){
-    console.log(`Name: ${this.name}`)
-}
-getId(){
-    console.log(`Id: ${this.id}`)
-}
-getTitle(){
-    console.log(`title: ${this.title}`)
-}
+    getName() {
+        console.log(`Name: ${this.name}`)
+    }
+    getId() {
+        console.log(`Id: ${this.id}`)
+    }
+    getTitle() {
+        console.log(`title: ${this.title}`)
+    }
 }
 //let manager = new Employee('sulu',12,'manager')
 //manager.getName()
 //manager.getId()
 //manager.getTitle()
 
-class Manager extends Employee{
-constructor(name,id,title,officeNumber){
-super(name,id,title)
-this.officeNumber = officeNumber
+class Manager extends Employee {
+    constructor(name, id, title, officeNumber) {
+        super(name, id, title)
+        this.officeNumber = officeNumber
+    }
+
+    getRole() {
+        console.log(`Role:${this.title}`)
+    }
 }
 
-getRole(){
-    console.log(`Role:${this.title}`)
-}
-}
-
-class Engineer extends Employee{
-    constructor(name,id,title,github){
-        super(name,id,title)
+class Engineer extends Employee {
+    constructor(name, id, title, github) {
+        super(name, id, title)
         this.github = github
     }
-    getGithub(){
+    getGithub() {
         console.log(`Github id:${github}`)
     }
-    getRole(){(`Role: ${title}`)
-}
+    getRole() {
+        (`Role: ${title}`)
+    }
 }
 
-class Intern extends Employee{
-    constructor(name,id,title,school){
-        super(name,id,title)
+class Intern extends Employee {
+    constructor(name, id, title, school) {
+        super(name, id, title)
         this.school = school
     }
-    getSchool(){
+    getSchool() {
         console.log(`School:${this.school}`)
     }
-    getRole(){
+    getRole() {
         console.log(`Role:${this.title}`)
     }
 }
@@ -71,7 +73,12 @@ function promptCreator() {
         {
             type: 'text',
             name: 'id',
-            message: 'Enter your id:'
+            message: 'Enter your id:',
+           // validate(input,answer){if(input === answer)
+            //{
+              //  return "Id is already taken enter new number "
+            //}}
+
         },
         {
             type: 'text',
@@ -82,91 +89,96 @@ function promptCreator() {
             type: 'list',
             name: 'choice1',
             message: 'Do you want to add your team member?',
-            choices: ['Yes', 'No']
-            },
+            choices: ['Yes', 'No', 'Exit']
+        },
         {
             type: 'list',
             name: 'choice2',
             message: 'Select the role of your team member',
-            choices: ['Engineer','Intern']
+            choices: ['Engineer', 'Intern']
         }
     ])
-}   
-        //choice:['manager','engineer','intern']
-        function generateHTML(answers) {
-            return `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-                <style>
-                    .div{
-                        background-color: rgb(245, 120, 131);
-                        text-align: center;
-                    }
-                    .shape{
-                        text-align: center;
-                        
-                    }
 
-                </style>
-            </head>
-            <body>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-            <div class="div"> <h1>My Team</h1></div>
-                <div class="row card small">
-                    <div class="col s12 m6">
-                      <div class="card blue darken-1">
-                        <div class="card-content white-text">
-                          <h3 class="card-title shape">${answers.title}</h3>
-                          <div class="shape">${answers.name}</div>
-                          <div class="shape">${answers.id}</div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                
-            </body>
-            </html>
-            `
-                }        
-
-        promptCreator()
-        
-    .then(function (answers) {
-       switch(choice2){
-           case 'Engineer':
-             inquirer.prompt( [{
-                   type: 'text',
-                   name: 'github',
-                   message:'Enter your github id:'
-                }
-            ])
-            break
-            case 'Intern':
-                inquirer.prompt( [{
-                      type: 'text',
-                      name: 'github',
-                      message:'Enter your school:'
-                   }
-               ])
-               break
+        .then(function (answer) {
+            if (answer.choice2 === 'Engineer') {
+                engCreator()
             }
-        //}
-        const html = generateHTML(answers)
-        return writeFileAsync('employee.html', html)
-    })
-    .then(function () {
-        console.log('Successfully wrote to html file')
-    })
-    .catch(function (err) {
-        console.log(err)
-    })
+            else if (answer.choice2 === 'Intern') {
+                internCreator()
+            }
+            else {
+                const Manager1 = new Manager(`${answer.name}`, `${answer.id}`, `${answer.title}`, `${answer.officeNumber}`)  
 
+                path.join('/', '/team.html')
+                fs.writeFile('team.html', JSON.stringify(Manager1), function(error) {
+                    if (error) { console.log(error) }
+                })
+                    ()
+            }
+
+        })
+
+
+        .catch(function (err) {
+            console.log(err)
+        })
+}
+promptCreator()
+
+
+
+function engCreator() {
+    return inquirer.prompt([{
+        type: 'text',
+        name: 'github',
+        message: 'Enter your github id:'
+    }])
+
+        .then(function (answer) {
+            const Engineer1 = new Engineer(`${answer.name}`, `${answer.id}`, `${answer.title}`, `${answer.github}`)
+            path.join('/', '/team.html','/engineer.html')
+            fs.writeFile('engineer.html', JSON.stringify(Engineer1), function (error) {
+                if (error) { console.log(error) }
+                else { console.log('Successfully wrote to html') }
+
+
+                
+            })
+        },
+            function (err) {
+                if (err) throw err;
+                console.log("Your Team was created successfully!");
+                // re-prompt the user for if they want to add more team members
+                promptCreator();
+            }
+
+        )
+}
+
+function internCreator() {
+    return inquirer.prompt([{
+        type: 'text',
+        name: 'github',
+        message: 'Enter your school:'
+
+    }])
+        .then(function (answer) {
+           // const Intern1 = new Intern(`${answer.name}`, `${answer.id}`, `${answer.title}`, `${answer.school}`)
+           const intern = answer
+            path.join('/', '/intern.html')
+            fs.writeFile('intern.html', (intern), function(error) {
+                if (error) { console.log(error) }
+            })
+
+        },
+            function (error) {
+                if (error) { console.log(err) };
+                console.log("Team created successfully!");
+                promptCreator();
+            }
+
+        )
+}
 
 
 
